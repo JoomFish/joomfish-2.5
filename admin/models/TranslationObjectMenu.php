e60aedd1aa15ffb489c3ddec9e027baa62200c82
klas <klas.berlic@gmail.com>
2012-02-27 11:00:03 +0100

    Unset home attribute on copy to solve #33 - Default Home unique

--- admin/models/TranslationObjectMenu.php
+++ admin/models/TranslationObjectMenu.php
@@ -130,6 +130,15 @@ class  TranslationObjectMenu extends TranslationObject
 		//$path = $table->rebuild($table->id,  $table->lft, $table->level, $newPath);;
 
 	}
+	
+	/*
+	 * Unset home attribute on copy, we can only have one home in single menu tree
+	*/
+	public function unsetHomeOr(&$row) {
+	
+		$row->home = "0";
+		$row->jfc_home = "0";
+	}
 
 }
 
